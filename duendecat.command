9ef30e98c517c30c54b73d481705ba30bd4c212d
patarapolw <patarapolw@gmail.com>
2018-01-11 22:45:15 +0700

    Add default value to -h

--- duendecat.command
+++ duendecat.command
@@ -1,5 +1,5 @@
-nohup $HOME/Documents/GitHub/duendecat/main.py --gui --level 99 --lang jp --sheet N3 &
-#nohup $HOME/Documents/GitHub/duendecat/main.py --gui --level 2 &
+#nohup $HOME/Documents/GitHub/duendecat/main.py --gui --level 99 --lang jp --sheet N3 &
+nohup $HOME/Documents/GitHub/duendecat/main.py --gui --level 2 &
 #$HOME/Documents/GitHub/duendecat/main.py
 #$HOME/Documents/GitHub/duendecat/main.py -h
 
@@ -11,20 +11,21 @@ nohup $HOME/Documents/GitHub/duendecat/main.py --gui --level 99 --lang jp --shee
 #
 #optional arguments:
 #  -h, --help            show this help message and exit
-#  --gui                 Tag if GUI
-#  --lang LANG           cn or jp
-#  --level LEVEL         Kanji/Hanzi level
-#  --sheet SHEET         Worksheet to read
-#  --times TIMES         Number of times to repeat (CLI)
-#  --lang-first          Lang JP/CN before EN
-#  --reverse             EN before Lang JP/CN
-#  --silent              No vocal output
-#  --speak               Vocal output
-#  --auto                Loop automatically
-#  --no-auto             Do not loop automatically
+#  --gui                 Tag if GUI (default: CLI)
+#  --lang LANG           cn or jp (default: cn)
+#  --level LEVEL         Kanji/Hanzi level (default: 5)
+#  --sheet SHEET         Worksheet to read (default: depends)
+#  --times TIMES         Number of times to repeat (CLI) (default: 60)
+#  --lang-first          Lang JP/CN before EN (default: reverse)
+#  --reverse             EN before Lang JP/CN (default: reverse)
+#  --silent              No vocal output (default: speak)
+#  --speak               Vocal output (default: speak)
+#  --auto                Loop automatically (default: auto)
+#  --no-auto             Do not loop automatically (default: auto)
 #  --show-answer-lapse SHOW_ANSWER_LAPSE
-#                        Lapse in seconds to show answer
+#                        Lapse in seconds to show answer (default: 3)
 #  --new-question-lapse NEW_QUESTION_LAPSE
-#                        Lapse in seconds to show new question
+#                        Lapse in seconds to show new question (default: 1)
 #  --speech-engine SPEECH_ENGINE
 #                        Set speech engine. "google" for google_speech
+#                        (default: not_set)
--- main.py
+++ main.py
@@ -5,23 +5,23 @@ import argparse
 logging.disable(logging.CRITICAL)
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--gui', help='Tag if GUI', action='store_true')
-parser.add_argument('--lang', help='cn or jp', default='cn')
-parser.add_argument('--level', help='Kanji/Hanzi level', default=5, type=int)
-parser.add_argument('--sheet', help='Worksheet to read', default='depends')
-parser.add_argument('--times', help='Number of times to repeat (CLI)', default=60, type=int)
-parser.add_argument('--lang-first', help='Lang JP/CN before EN', action='store_false', dest='is_reverse')
-parser.add_argument('--reverse', help='EN before Lang JP/CN', action='store_true', dest='is_reverse')
+parser.add_argument('--gui', help='Tag if GUI (default: CLI)', action='store_true')
+parser.add_argument('--lang', help='cn or jp (default: %(default)s)', default='cn')
+parser.add_argument('--level', help='Kanji/Hanzi level (default: %(default)s)', default=5, type=int)
+parser.add_argument('--sheet', help='Worksheet to read (default: %(default)s)', default='depends')
+parser.add_argument('--times', help='Number of times to repeat (CLI) (default: %(default)s)', default=60, type=int)
+parser.add_argument('--lang-first', help='Lang JP/CN before EN (default: reverse)', action='store_false', dest='is_reverse')
+parser.add_argument('--reverse', help='EN before Lang JP/CN (default: reverse)', action='store_true', dest='is_reverse')
 parser.set_defaults(is_reverse=True)
-parser.add_argument('--silent', help='No vocal output', action='store_false', dest='speak')
-parser.add_argument('--speak', help='Vocal output', action='store_true', dest='speak')
+parser.add_argument('--silent', help='No vocal output (default: speak)', action='store_false', dest='speak')
+parser.add_argument('--speak', help='Vocal output (default: speak)', action='store_true', dest='speak')
 parser.set_defaults(speak=True)
-parser.add_argument('--auto', help='Loop automatically', action='store_true', dest='auto')
-parser.add_argument('--no-auto', help='Do not loop automatically', action='store_false', dest='auto')
+parser.add_argument('--auto', help='Loop automatically (default: auto)', action='store_true', dest='auto')
+parser.add_argument('--no-auto', help='Do not loop automatically (default: auto)', action='store_false', dest='auto')
 parser.set_defaults(auto=True)
-parser.add_argument('--show-answer-lapse', help='Lapse in seconds to show answer', default=3, type=int)
-parser.add_argument('--new-question-lapse', help='Lapse in seconds to show new question', default=1, type=int)
-parser.add_argument('--speech-engine', help='Set speech engine. "google" for google_speech', default='not_set')
+parser.add_argument('--show-answer-lapse', help='Lapse in seconds to show answer (default: %(default)s)', default=3, type=int)
+parser.add_argument('--new-question-lapse', help='Lapse in seconds to show new question (default: %(default)s)', default=1, type=int)
+parser.add_argument('--speech-engine', help='Set speech engine. "google" for google_speech (default: %(default)s)', default='not_set')
 
 
 #parser.print_help()
@@ -29,7 +29,7 @@ arg = parser.parse_args()
 param = vars(arg)
 #print(arg)
 
-param['gui'] = True
+#param['gui'] = True
 
 if param['sheet'] == 'default':
 	if param['lang'] == 'jp':
